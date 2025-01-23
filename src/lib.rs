f94391af7acac0bebfe7b22c37237605e80e1435
Kadek Widi <kdwidi@pm.me>
2025-01-23 16:15:58 +0700

    add retrieval scheme and randomness testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-pub use patterns::*;
+pub use schemes::*;
+use statrs::function::{erf::erfc, gamma::gamma_ur};
 
-mod patterns;
+mod schemes;
 
-const BIT_SIZE: usize = 64;
-
-pub fn entry_scheme(bits: &[u8], pattern: &Pattern) -> Vec<u8> {
+pub fn entry_scheme_transpose(bits: &[u8], pattern: &Scheme) -> Vec<u8> {
     let mut result = Vec::new();
     for index in pattern {
         result.push(bits[*index as usize]);
@@ -12,7 +11,7 @@ pub fn entry_scheme(bits: &[u8], pattern: &Pattern) -> Vec<u8> {
     result
 }
 
-pub fn retrieval_scheme(bits: &[u8], pattern: &Pattern) -> Vec<u8> {
+pub fn retrieval_scheme_transpose(bits: &[u8], pattern: &Scheme) -> Vec<u8> {
     let mut result = vec![0; 64];
     for (i, index) in pattern.iter().enumerate() {
         result[*index as usize] = bits[i];
@@ -20,25 +19,104 @@ pub fn retrieval_scheme(bits: &[u8], pattern: &Pattern) -> Vec<u8> {
     result
 }
 
-pub fn print_all(plaintext: &[u8], entry: &[u8], retrieval: &[u8]) {
-    for i in 0..8 {
-        for j in 0..8 {
-            print!("{: >2}, ", plaintext[(i * 8) + j]);
-        }
-        print!("  ");
-        for j in 0..8 {
-            print!("{: >2}, ", entry[(i * 8) + j]);
-        }
-        print!("  ");
-        let mut byte = String::new();
-        for j in 0..8 {
-            print!("{: >2}, ", retrieval[(i * 8) + j]);
-            byte.push_str(&format!("{}", retrieval[(i * 8) + j]));
-        }
-        if let Ok(num) = u8::from_str_radix(&byte, 2) {
-            print!("  => {byte} | {num: >3}",);
-        }
-        println!();
+pub fn monobit(bits: &[u8]) -> f64 {
+    let n = bits.len();
+    let mut sn = 0_i32;
+    for bit in bits {
+        sn += *bit as i32 * 2 - 1;
     }
-    println!();
+    let s = sn.abs() as f64 / (n as f64).sqrt();
+    erfc(s / 2_f64.sqrt())
+}
+
+#[allow(non_snake_case)]
+pub fn blockbit(bits: &[u8], M: usize) -> f64 {
+    let n = bits.len();
+    let N = n / M;
+
+    let mut pis = vec![];
+    for i in 0..N {
+        let mut ones = 0;
+        for j in 0..M {
+            if bits[(i * M) + j] == 1 {
+                ones += 1
+            }
+        }
+        pis.push(ones as f64 / M as f64);
+        if pis.len() == N {
+            break;
+        }
+    }
+    let pis = pis
+        .into_iter()
+        .map(|ones| (ones - 0.5).powf(2_f64))
+        .sum::<f64>();
+
+    let x2 = (4 * M) as f64 * pis;
+    gamma_ur(N as f64 / 2.0, x2 / 2.0)
+}
+
+pub fn runs(bits: &[u8]) -> f64 {
+    let n = bits.len();
+    let pi = bits.iter().filter(|b| **b == 1).count() as f64 / n as f64;
+    let tau = 2.0 / (n as f64).sqrt();
+    if (pi - 0.5).abs() >= tau {
+        return 0.0;
+    }
+    let mut vn: usize = 1;
+    for i in 0..n - 1 {
+        if bits[i] == bits[i + 1] {
+            vn += 0;
+        } else {
+            vn += 1;
+        }
+    }
+    let numerator = (vn as f64 - 2.0 * n as f64 * pi * (1.0 - pi)).abs();
+    let denominator = 2.0 * (2.0 * n as f64).sqrt() * pi * (1.0 - pi);
+    erfc(numerator / denominator)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::{blockbit, monobit, runs};
+
+    fn str_bit_to_vec_u8(input: &str) -> Vec<u8> {
+        input
+            .chars()
+            .map(|c| if c == '1' { 1 } else { 0 })
+            .collect::<Vec<u8>>()
+    }
+
+    #[test]
+    fn monobit_test() {
+        let input = str_bit_to_vec_u8("1011010101");
+        assert_eq!(&format!("{:.6}", monobit(&input)), "0.527089");
+
+        let input = str_bit_to_vec_u8(
+            "1100100100001111110110101010001000100001011010001100001000110100110001001100011001100010100010111000",
+        );
+        assert_eq!(&format!("{:.6}", monobit(&input)), "0.109599");
+    }
+
+    #[test]
+    fn blockbit_test() {
+        let input = str_bit_to_vec_u8("0110011010");
+        assert_eq!(&format!("{:.6}", blockbit(&input, 3)), "0.801252");
+
+        let input = str_bit_to_vec_u8(
+            "1100100100001111110110101010001000100001011010001100001000110100110001001100011001100010100010111000",
+        );
+        assert_eq!(&format!("{:.6}", blockbit(&input, 10)), "0.706438");
+    }
+
+    #[test]
+    fn runs_test() {
+        let input = str_bit_to_vec_u8("1001101011");
+        assert_eq!(&format!("{:.6}", runs(&input)), "0.147232");
+        let input = str_bit_to_vec_u8(
+            "1100100100001111110110101010001000100001011010001100001000110100110001001100011001100010100010111000",
+        );
+        assert_eq!(&format!("{:0.6}", runs(&input)), "0.500798");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,118 @@
 use major_scale_square_transposition::*;
 
+const PLAINTEXT: &str = "FTI UKSW";
+
 fn main() {
-    // let plaintext = str_to_binary("FTI UKSW");
-    let plaintext = (0..64).collect::<Vec<u8>>();
+    real_deal_mode();
+}
 
-    let patterns = [PATTERN_1, PATTERN_2, PATTERN_3, PATTERN_4];
+#[allow(dead_code)]
+fn real_deal_mode() {
+    let plaintext = str_to_binary(PLAINTEXT);
+    // #[rustfmt::skip]
+    // let plaintext: Vec<u8> = vec![
+    //     1, 1, 1, 1, 1, 1, 1, 1,
+    //     0, 0, 0, 0, 0, 0, 0, 0,
+    //     1, 1, 1, 1, 1, 1, 1, 1,
+    //     0, 0, 0, 0, 0, 0, 0, 0,
+    //     1, 1, 1, 1, 1, 1, 1, 1,
+    //     0, 0, 0, 0, 0, 0, 0, 0,
+    //     1, 1, 1, 1, 1, 1, 1, 1,
+    //     0, 0, 0, 0, 0, 0, 0, 1,
+    // ];
 
-    for (i, c) in patterns.iter().enumerate() {
-        for (j, c2) in patterns.iter().enumerate() {
-            if i == j {
-                continue;
-            }
+    let mut test_result = vec![];
+    for (i, e) in ENTRY_SCHEMES.iter().enumerate() {
+        for (j, r) in RETRIEVAL_SCHEMES.iter().enumerate() {
+            let entry = entry_scheme_transpose(&plaintext, e);
+            let retrieval = retrieval_scheme_transpose(&entry, r);
 
-            let entry_scheme = entry_scheme(&plaintext, c);
-            let retrieval_scheme = retrieval_scheme(&entry_scheme, c2);
-            println!(
-                "PLAINTEXT\t\t\t   ENTRY SCHEME {}\t\t     RETRIEVAL SCHEME {}",
-                i + 1,
-                j + 1
-            );
-            print_all(&plaintext, &entry_scheme, &retrieval_scheme);
+            let monobit = monobit(&retrieval);
+            let blockbit = blockbit(&retrieval, 8);
+            let runs = runs(&retrieval);
+
+            test_result.push((i, j, monobit, blockbit, runs));
         }
+    }
+
+    test_result.sort_by(|(_, _, _, _, a), (_, _, _, _, b)| b.total_cmp(a));
+
+    #[allow(clippy::needless_range_loop)]
+    for i in 0..4 {
+        let (ei, ri, monobit, blockbit, runs) = test_result[i];
+        let entry = entry_scheme_transpose(&plaintext, &ENTRY_SCHEMES[ei]);
+        let retrieval = retrieval_scheme_transpose(&entry, &RETRIEVAL_SCHEMES[ri]);
+        print_full(&plaintext, &entry, &retrieval, ei, ri);
+
+        println!(
+            "plainbit  : {}",
+            plaintext
+                .iter()
+                .enumerate()
+                .map(|(i, b)| if (i + 1) % 8 == 0 {
+                    format!("{b} ")
+                } else {
+                    b.to_string()
+                })
+                .collect::<String>()
+        );
+        println!(
+            "transposed: {}",
+            retrieval
+                .iter()
+                .enumerate()
+                .map(|(i, b)| if (i + 1) % 8 == 0 {
+                    format!("{b} ")
+                } else {
+                    b.to_string()
+                })
+                .collect::<String>()
+        );
+        println!();
+
+        println!("test results:");
+        println!("=> monobit : {:.6}", monobit);
+        println!("=> blockbit: {:.6}", blockbit);
+        println!("=> runs    : {:.6}\n\n", runs);
     }
 }
 
-fn monobit_test(bits: &[u8]) {
-    let n = bits.len();
-    let mut sn = 0_i32;
-    for bit in bits {
-        sn += if *bit == 0 { -1 } else { 1 };
+#[allow(dead_code)]
+fn index_mode(entry: &[u8; 64], retrieval: &[u8; 64], ei: usize, ri: usize) {
+    let plaintext = (0..64).collect::<Vec<u8>>();
+    let entry = entry_scheme_transpose(&plaintext, entry);
+    let retrieval = retrieval_scheme_transpose(&entry, retrieval);
+    print_full(&plaintext, &entry, &retrieval, ei, ri);
+}
+
+fn print_full(plaintext: &[u8], entry: &[u8], retrieval: &[u8], ei: usize, ri: usize) {
+    println!(
+        "plaintext \"FTI UKSW\"              \
+        entry {: >2}                          \
+        retrieval {: >2}",
+        ei + 1,
+        ri + 1,
+    );
+    for i in 0..8 {
+        for j in 0..8 {
+            print!("{: >2}, ", plaintext[(i * 8) + j]);
+        }
+        print!("  ");
+        for j in 0..8 {
+            print!("{: >2}, ", entry[(i * 8) + j]);
+        }
+        print!("  ");
+        // let mut byte = String::new();
+        for j in 0..8 {
+            print!("{: >2}, ", retrieval[(i * 8) + j]);
+            // byte.push_str(&format!("{}", retrieval[(i * 8) + j]));
+        }
+        // if let Ok(num) = u8::from_str_radix(&byte, 2) {
+        //     print!("  => {byte} | {num: >3}",);
+        // }
+        println!();
     }
-    let sobs = sn.abs() as f64 / (n as f64).sqrt();
-    // fix erfc formula
-    let erfc = sobs / 2_f64.sqrt();
-    dbg!(n, sn, sobs, erfc);
+    println!();
 }
 
 fn str_to_binary(input: &str) -> Vec<u8> {
@@ -51,11 +130,43 @@ fn str_to_binary(input: &str) -> Vec<u8> {
     result
 }
 
-fn print_bits(bits: &[u8]) {
+pub fn print_bits(bits: &[u8]) {
     for (i, bit) in bits.iter().enumerate() {
         print!("{bit},");
         if i > 0 && (i + 1) % 8 == 0 {
             println!();
         }
+    }
+}
+
+#[allow(dead_code)]
+fn print_indexes(entry: &[u8], retrieval: &[u8]) {
+    for (i, e) in entry.iter().enumerate() {
+        print!("{},", e + 1);
+        if (i + 1) % 8 == 0 {
+            println!();
+        }
+    }
+    println!();
+
+    let mut result = vec![0; 64];
+    for (i, e) in entry.iter().enumerate() {
+        result[retrieval[i] as usize] = *e;
+        // print!("{: >2} ({: >2}), ", e + 1, retrieval[i] + 1);
+        print!("{} ({}), ", e + 1, retrieval[i] + 1);
+        if (i + 1) % 8 == 0 {
+            println!();
+        }
+    }
+
+    for (i, r) in result.iter().enumerate() {
+        print!("{},", r + 1);
+        if (i + 1) % 8 == 0 {
+            println!();
+        }
+    }
+
+    for i in &result {
+        println!("{}", i + 1);
     }
 }
